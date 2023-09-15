a649db642be161f95d5e1381ad03ca0a8c03478a
Динара Гиндуллинa <59680253+Dinara1234@users.noreply.github.com>
2023-09-16 00:05:43 +0300

    Add files via upload

---
+++ dz_2_12.java
@@ -0,0 +1,11 @@
+public class dz_2_12 {
+    public static void main(String[] args) {
+        int i;
+        int j;
+        for (i = 6; i>1; i-=2){
+            for (j =0; j>0; j--){
+                System.out.println(j*" " + "!"*i)
+            }
+        }
+    }
+}
---
+++ dz_2_9.java
@@ -0,0 +1,7 @@
+public class dz_2_9 {
+    public static void main(String[] args) {
+        int x = 9;
+        System.out.println("значение х равно " + x);
+        System.out.println("квадрат х равно " + x*x);
+    }
+}
