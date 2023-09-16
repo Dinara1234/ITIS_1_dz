40cd41f63b3512e46370d45000ff197a3c9171f7
Динара Гиндуллинa <59680253+Dinara1234@users.noreply.github.com>
2023-09-16 10:15:36 +0300

    Add files via upload

---
+++ dz_2_8.java
@@ -0,0 +1,9 @@
+public class dz_2_8 {
+    public static void main(String[] args) {
+        int x = 6;
+        System.out.println(x);
+        System.out.println(x*x);
+        System.out.println(x*x*x);
+
+    }
+}
