d66903f37b456521e04aff0443ada3b343debe57
Динара Гиндуллинa <59680253+Dinara1234@users.noreply.github.com>
2023-09-16 10:14:38 +0300

    Add files via upload

---
+++ dz_2_21.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class dz_2_21 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        System.out.println(x+1);
+        System.out.println(x+2);
+        System.out.println(x+3);
+    }
+
+}
